f162a372886b179ad192f6f44e93b48a8112c40b
dongyu.zhao <stupid_zhaodongyu@hotmail.com>
2024-09-14 16:49:35 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
-
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-    console.log(Data);
-    console.log(Notes);
+    const btn = document.createElement('button');
 
-  
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
   }
   
---
+++ src01/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
+
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    console.log(Data);
+    console.log(Notes);
+
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
