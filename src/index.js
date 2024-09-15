d8f5630caef499d8d3532cd0e568ede4feef66ce
dongyu.zhao <stupid_zhaodongyu@hotmail.com>
2024-09-15 19:13:23 +0800

    Tree shaking

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash'
+import { cube } from './math';
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('pre');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to' + cube(5)
+  ].join('\n\n');
+
+  return element;
+}
+
+document.body.appendChild(component());
