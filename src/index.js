9f506de690f2799de23e108f7016bf78f6cc66b5
dongyu.zhao <stupid_zhaodongyu@hotmail.com>
2024-09-15 20:26:39 +0800

    Production

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import _ from 'lodash'
 import { cube } from './math';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('development mode');
+}
 function component() {
+
   const element = document.createElement('pre');
 
   // Lodash, currently included via a script, is required for this line to work
@@ -12,5 +17,6 @@ function component() {
 
   return element;
 }
+
 
 document.body.appendChild(component());
